7dc6599743f83588de6d2980c332fa16c14e2a37
scur0503 <scur0503@snellius.surf.nl>
2026-05-20 00:12:45 +0200

    export graph_evaluation package

---
+++ graph_evaluation/__init__.py
@@ -0,0 +1,16 @@
+"""
+Graph evaluation utilities.
+
+Run:
+    python -c "from graph_evaluation import Evaluator, GraphEvaluatorCallback"
+"""
+
+from .degree_mmd import DegreeMMD
+from .evaluator import Evaluator
+from .trainer_callback import GraphEvaluatorCallback
+
+__all__ = [
+    "DegreeMMD",
+    "Evaluator",
+    "GraphEvaluatorCallback",
+]
